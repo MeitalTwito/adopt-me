b6fa327977696f8f022be95aede44fe09d66646c
Meital Zamir (Twito) <tw.meital@gmail.com>
2023-09-06 16:15:19 +0300

    added props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese"),
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
   ]);
 };
 
@@ -15,9 +15,21 @@ const App = () => {
     // children
     [
       React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet),
+      React.createElement(Pet, {
+        animal: "Dog",
+        name: "Luna",
+        breed: "Havanese",
+      }),
+      React.createElement(Pet, {
+        animal: "Bird",
+        name: "Pepper",
+        breed: "Cockatiel",
+      }),
+      React.createElement(Pet, {
+        animal: "Cat",
+        name: "Doink",
+        breed: "Mixed",
+      }),
     ]
   );
 };
