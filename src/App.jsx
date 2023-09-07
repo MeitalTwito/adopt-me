3047f3bd04742468d1efd10d2f392d11394ba2a1
Meital Zamir (Twito) <tw.meital@gmail.com>
2023-09-07 14:16:30 +0300

    implementing vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom";
+import Pet from "./pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cickatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
