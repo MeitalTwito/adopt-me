68701724b2c2143b550a2c2504fd77ba38511307
Meital Zamir (Twito) <tw.meital@gmail.com>
2023-09-07 16:58:51 +0300

    Added Pets API connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import ReactDOM from "react-dom";
-import Pet from "./pet";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cickatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(<App />);
