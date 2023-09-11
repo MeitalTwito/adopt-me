6acec81ffc33a1284b58070f97ab6efe5df83e86
Meital Zamir (Twito) <tw.meital@gmail.com>
2023-09-11 13:46:45 +0300

    implemented uncontrolled form

--- src/fetchPet.js
+++ src/fetchPet.js
@@ -1,6 +1,6 @@
 const fetchPet = async ({ queryKey }) => {
   const id = queryKey[1];
-  const region = queryKey[2];
+  //   const region = queryKey[2];
   const apiRes = await fetch(`http://pets-v2.dev-apis.com/pets?id=${id}`);
 
   if (!apiRes.ok) {
